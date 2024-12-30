3f38d2b817631f7d5b6ff1824a71b683fbe39dce
adarsh-iiit <adarshtech251@gmail.com>
2024-12-30 12:44:43 +0530

    a simple basic template project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+
+
+import { Link } from 'react-router-dom'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-auto">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
